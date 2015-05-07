5077120f6c1f7e97bc92cd48c2f8529acde67241
Bobby Fata <bobbyfata@gmail.com>
2015-05-07 14:21:02 -0400

    First Commit

---
+++ Assets/BlipControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlipControl : MonoBehaviour {
+	 Renderer rend;
+	public Material colorChange;
+	public Material defaultColor;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	void OnMouseEnter() {
+		rend.material = colorChange;
+	}
+
+	void OnMouseExit () {
+		rend.material = defaultColor;
+	
+	}
+}
