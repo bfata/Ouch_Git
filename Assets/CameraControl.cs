157dca98ea2c426c61390187f9f4edd55156d059
Bobby Fata <bobbyfata@gmail.com>
2015-05-20 15:57:06 -0400

    Camera look to information

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void wristLook () {
+		iTween.RotateBy(gameObject, iTween.Hash("y", .25, "easeType", "easeInOutQuad"));
+	}
+
+	public void wristLookBack () {
+		iTween.RotateBy(gameObject, iTween.Hash("y", -.25, "easeType", "easeInOutQuad"));
+	}
+
+	public void kneeLook () {
+		iTween.RotateBy(gameObject, iTween.Hash("x", .25, "easeType", "easeInOutQuad"));
+	}
+
+	public void kneeLookBack () {
+		iTween.RotateBy(gameObject, iTween.Hash("x", -.25, "easeType", "easeInOutQuad"));
+	}
+}
